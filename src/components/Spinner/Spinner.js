2ef2c5493a5e45e43f146c8fcd4a260959982809
Paul <gasior002@gmail.com>
2019-05-13 14:20:28 +0200

    css - fix background in spinner, design order cards

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -2,9 +2,11 @@ import React from 'react';
 import classes from './Spinner.css'
 
 const spinner = (props) => {
-    return (
-        <div className={classes.Spinner}>{props.children}</div>
-    );
+	return (
+		<div className={classes.SpinnerContainer}>
+			<div className={classes.Spinner}>{props.children}</div>
+		</div>
+	);
 }
 
 export default spinner;
